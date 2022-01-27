0c6b13b35852182754579018644d73b42adfa646
dmansdman <dmansdman@gmail.com>
2022-01-26 21:08:38 -0500

    Restored timezone to LA

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ import {MAPPED_AFK_CHECK_REACTIONS} from "./constants/dungeons/MappedAfkCheckRea
 process.on("unhandledRejection", e => {
     console.error(
         new StringBuilder()
-            .append(`[${TimeUtilities.getDateTime(Date.now(), "America/New_York")}] ${e}`)
+            .append(`[${TimeUtilities.getDateTime(Date.now(), "America/Los_Angeles")}] ${e}`)
             .appendLine()
             .append("=====================================")
             .toString()
@@ -38,7 +38,7 @@ process.on("unhandledRejection", e => {
 process.on("uncaughtException", e => {
     console.error(
         new StringBuilder()
-            .append(`[${TimeUtilities.getDateTime(Date.now(), "America/New_York")}] ${e.name}`)
+            .append(`[${TimeUtilities.getDateTime(Date.now(), "America/Los_Angeles")}] ${e.name}`)
             .appendLine()
             .append(e.message)
             .appendLine(2)
