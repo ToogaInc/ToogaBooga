ee01f9cd543cae6214910180c931d238800e1517
dmansdman <dmansdman@gmail.com>
2022-03-28 15:05:39 -0400

    ARL can clean/yoink.

--- src/commands/staff/YoinkVC.ts
+++ src/commands/staff/YoinkVC.ts
@@ -16,6 +16,7 @@ export class YoinkVC extends BaseCommand {
                 "Security",
                 "Officer",
                 "Moderator",
+                "AlmostRaidLeader",
                 "RaidLeader",
                 "HeadRaidLeader",
                 "VeteranRaidLeader"
