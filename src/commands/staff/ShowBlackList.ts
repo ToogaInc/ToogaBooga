713116be2dd51a50c4301c3708a524065147bb15
SaifSushi <alushisaif@gmail.com>
2022-06-28 02:27:36 -0500

    ahhhh

--- src/commands/staff/ShowBlackList.ts
+++ src/commands/staff/ShowBlackList.ts
@@ -1,18 +1,15 @@
-import { ArgumentType, BaseCommand, ICommandContext, ICommandInfo } from "../BaseCommand";
-import { SlashCommandBuilder } from "@discordjs/builders";
+import { BaseCommand, ICommandContext, ICommandInfo } from "../BaseCommand";
 import { MessageUtilities } from "../../utilities/MessageUtilities";
 import { StringBuilder } from "../../utilities/StringBuilder";
-import { StringUtil } from "../../utilities/StringUtilities";
-import { TimeUtilities } from "../../utilities/TimeUtilities";
 
-export class ShowBlackList extends BaseCommand {
+export class ShowBlacklist extends BaseCommand {
     public constructor() {
         const cmi: ICommandInfo = {
             cmdCode: "SHOW_BLACKLIST",
             formalCommandName: "Show Blacklist",
             botCommandName: "showblacklist",
             description: "Shows a list of all current blacklisted users of the server",
-            rolePermissions: ["Officer", "Moderator", "HeadRaidLeader"],
+            rolePermissions: ["Officer", "Moderator", "HeadRaidLeader", "Security"],
             generalPermissions: [],
             botPermissions: [],
             commandCooldown: 3 * 1000,
@@ -30,18 +27,21 @@ export class ShowBlackList extends BaseCommand {
     //add command to index of commands and bot.ts
     public async run(ctx: ICommandContext): Promise<number> {
         const limit = 4096;
-        const blInfo = ctx.guildDoc?.moderation.blacklistedUsers;
+        const blInfo = ctx.guildDoc?.moderation.blacklistedUsers
+        .filter(x => !x.realmName.lowercaseIgn.includes("a"))
+        
         if (!blInfo) {
             await ctx.interaction.reply({
-                content: "Could not retrieve blacklisted members"
+                content: "Could not retrieve blacklisted members."
             });
-
-            return 0;
+            return -1;
         }
+
         else if (blInfo.length > limit){
             await ctx.interaction.reply({
-                content: "List too big yell at devs."
+                content: "List of blacklisted members is too large."
             });
+            return -1;
         }
         
         const embed = MessageUtilities.generateBlankEmbed(ctx.guild!, "RED")
